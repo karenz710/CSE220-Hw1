7f3249dee677cdd49afa6a30971da980a96fe54d
Karen Zhao <146024684+karenz710@users.noreply.github.com>
2025-02-23 04:40:07 +0000

    commit

--- include/hw1.h
+++ include/hw1.h
@@ -41,7 +41,7 @@ Part 2
 
 void edge_clue_initialization(void);
 */
-void apply_process_of_elimination(int row, int column, int piece);
+bool apply_process_of_elimination(int row, int column, int piece);
 /*
 Part 3
 */
